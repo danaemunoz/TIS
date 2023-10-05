ccb12ab2ede8c68093848f28fc59821796dedb1b
Javiera <jmunozv@ing.ucsc.cl>
2023-10-05 11:48:50 -0300

    PHP

--- PHP/ejercicio_funciones/index.php
+++ PHP/ejercicio_funciones/index.php
@@ -60,25 +60,30 @@ $respuesta = mysqli_query($conexion, $consulta);
                             echo "<td>" . $row["nombre"] . "</td>";
                             echo "<td>" . $row["origen"] . "</td>";
                             echo "<td>" . $row["logo"] . "</td>";
+
                             echo "<td>";
                             echo "<a href= 'eliminar.php?id_enviado=" . $row["id"] . "'>";
                             echo "<button class=' btn btn-sm'> Eliminar</button>";
                             echo "</a>";
                             echo "</td>";
+
+
+                            echo "<td>";
+                            echo "<a href= 'editar.php?id_enviado=" . $row["id"] . "'>";
+                            echo "<button class=' btn btn-sm'> Editar</button>";
+                            echo "</a>";
+                            echo "</td>";
+
+
+
                             echo "</tr>";
                         }
-
                         ?>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-
-
-
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
